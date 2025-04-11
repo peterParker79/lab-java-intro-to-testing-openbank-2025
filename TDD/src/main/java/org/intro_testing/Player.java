a21663d0a6cdda7919abe4442ae9055ee494d475
peterParker79 <trypeterparker@gmail.com>
2025-04-11 20:46:58 +0200

    Created abstract class Player

--- TDD/src/main/java/org/intro_testing/Player.java
+++ TDD/src/main/java/org/intro_testing/Player.java
@@ -5,10 +5,17 @@ public abstract class Player {
     private int strength;
     private int lives;
 
+    private final int  initialHealth;
+
     public Player(int health, int strength, int lives) {
         setHealth(health);
         setStrength(strength);
         setLives(lives);
+        initialHealth=health;
+    }
+
+    public int getInitialHealth() {
+        return initialHealth;
     }
 
     public int getHealth() {
@@ -33,5 +40,23 @@ public abstract class Player {
 
     public void setLives(int lives) {
         this.lives = lives;
+    }
+
+    public void decrementLive (){
+        this.lives-=1;
+        this.health=this.initialHealth;
+        if (lives<=0){
+            System.out.println("This character is dead");
+        }
+    }
+
+    public void attack (Player playerToAttack){
+        playerToAttack.setHealth(this.strength-playerToAttack.getHealth());
+    }
+
+    public void checkHealth() {
+        if(this.health<=0){decrementLive();}
+
+
     }
 }
